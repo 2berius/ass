71aab9547cd41024d79a93fe9959100a81a824f8
2berius <90366044+2berius@users.noreply.github.com>
2022-04-28 14:18:11 -0700

    Update ass.py

--- ass.py
+++ ass.py
@@ -12,18 +12,18 @@ def main():
     except:
         print("[!] Update chromedriver [!]")
 
-    _ = input('Enter anything once you log in to your Google account: ')
+    _ = input("Enter anything once you log in to your Google account: ")
 
     while 1:
         time.sleep(1)
         driver.find_element_by_xpath(
-            '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/span/div/div[2]/label/div/div[1]/div/div[3]/div').click()
+            "/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/span/div/div[2]/label/div/div[1]/div/div[3]/div").click()
         driver.find_element_by_xpath(
-            '/html/body/div[1]/div[2]/form/div[2]/div/div[3]/div/div[1]/div/span/span').click()
+            "/html/body/div[1]/div[2]/form/div[2]/div/div[3]/div/div[1]/div/span/span").click()
         time.sleep(1)
         driver.find_element_by_xpath(
-            '/html/body/div[1]/div[2]/div[1]/div/div[4]/a').click()
+            "/html/body/div[1]/div[2]/div[1]/div/div[4]/a").click()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
